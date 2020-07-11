72080a6187e6806c6a7fdd5984cb631e3768b7a5
sebasmos <ulsordonez@unicauca.edu.co>
2020-07-11 18:29:21 -0500

    dbs not nulls

--- act_disaggregated_dyspnea_records_vacc_records.sql
+++ act_disaggregated_dyspnea_records_vacc_records.sql
@@ -1,24 +1,27 @@
 CREATE TABLE dyspnea(
-    id_patient INTEGER NOT NULL,
-    discharge_date DATE,
-    medical_test INTEGER,
+    id_patient INTEGER NOT NULL, 
+    id_record: INTEGER NOT NULL,
+    discharge_date DATE NOT NULL,
+    medical_test INTEGER NOT NULL,
 );
 
 CREATE TABLE vaccination(
     id_patient INTEGER NOT NULL,
-    emission_date DATE,
+    id_record: INTEGER NOT NULL,
+    emission_date DATE NOT NULL,
     regional_eps VARCHAR(50),
-    eps_code INTEGER,
+    eps_code INTEGER NOT NULL,
     service_description VARCHAR(100),
-    diagnostic_eps_code INTEGER,
-    diagnostic_eps_description VARCHAR(100),
-    auth_quantity INTEGER
+    diagnostic_eps_code INTEGER NOT NULL,
+    diagnostic_eps_description VARCHAR(100) NOT NULL,
+    auth_quantity INTEGER NOT NULL
 );
 
 CREATE TABLE Act_Disaggregated(
     id_patient: INTEGER NOT NULL,
-    surver_name: VARCHAR(50),
-    Question_test: INTEGER,
-    Result: VARCHAR(50),
-    Result_Date: DATE
+    id_record: INTEGER NOT NULL,
+    surver_name: VARCHAR(50) NOT NULL,
+    Question_test: INTEGER NOT NULL,
+    Result: VARCHAR(50) NOT NULL,
+    Result_Date: DATE NOT NULL
 );
